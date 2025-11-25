89f567f9eb46866088e675c59895a4905a8cbff2
Roger Alexander Aldana Garcia <rogeraag.dev@gmail.com>
2025-11-25 14:31:13 -0500

    Creacion de botones de productos

--- SisGestionCafeteriaBuenGranito/FrmCaja.Designer.cs
+++ SisGestionCafeteriaBuenGranito/FrmCaja.Designer.cs
@@ -35,6 +35,20 @@
             this.btnCobrar = new System.Windows.Forms.Button();
             this.btnEliminar = new System.Windows.Forms.Button();
             this.pnl1 = new System.Windows.Forms.Panel();
+            this.btnAmericano = new System.Windows.Forms.Button();
+            this.btnCapuchino = new System.Windows.Forms.Button();
+            this.btnLatte = new System.Windows.Forms.Button();
+            this.btnChocolateCaliente = new System.Windows.Forms.Button();
+            this.btnInfusion = new System.Windows.Forms.Button();
+            this.btnJugo = new System.Windows.Forms.Button();
+            this.btnFrapuccino = new System.Windows.Forms.Button();
+            this.btnIcedCoffee = new System.Windows.Forms.Button();
+            this.btnAguaMineral = new System.Windows.Forms.Button();
+            this.btnGaseosa = new System.Windows.Forms.Button();
+            this.btnSandwichPollo = new System.Windows.Forms.Button();
+            this.btnMocaccino = new System.Windows.Forms.Button();
+            this.btnEmpanada = new System.Windows.Forms.Button();
+            this.btnMuffin = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvPedido)).BeginInit();
             this.pnl1.SuspendLayout();
             this.SuspendLayout();
@@ -42,9 +56,9 @@
             // dgvPedido
             // 
             this.dgvPedido.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvPedido.Location = new System.Drawing.Point(24, 82);
+            this.dgvPedido.Location = new System.Drawing.Point(16, 11);
             this.dgvPedido.Name = "dgvPedido";
-            this.dgvPedido.Size = new System.Drawing.Size(508, 345);
+            this.dgvPedido.Size = new System.Drawing.Size(296, 333);
             this.dgvPedido.TabIndex = 0;
             // 
             // lblTotal
@@ -52,7 +66,7 @@
             this.lblTotal.AutoSize = true;
             this.lblTotal.Font = new System.Drawing.Font("Century Gothic", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblTotal.ForeColor = System.Drawing.SystemColors.Control;
-            this.lblTotal.Location = new System.Drawing.Point(46, 353);
+            this.lblTotal.Location = new System.Drawing.Point(76, 388);
             this.lblTotal.Name = "lblTotal";
             this.lblTotal.Size = new System.Drawing.Size(148, 38);
             this.lblTotal.TabIndex = 1;
@@ -61,29 +75,29 @@
             // btnCafe
             // 
             this.btnCafe.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCafe.Location = new System.Drawing.Point(24, 22);
+            this.btnCafe.Location = new System.Drawing.Point(12, 12);
             this.btnCafe.Name = "btnCafe";
-            this.btnCafe.Size = new System.Drawing.Size(104, 35);
+            this.btnCafe.Size = new System.Drawing.Size(110, 100);
             this.btnCafe.TabIndex = 2;
-            this.btnCafe.Text = "Café";
+            this.btnCafe.Text = "Espresso";
             this.btnCafe.UseVisualStyleBackColor = true;
             this.btnCafe.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btnCafe_MouseClick);
             // 
             // btnCroissant
             // 
             this.btnCroissant.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCroissant.Location = new System.Drawing.Point(134, 22);
+            this.btnCroissant.Location = new System.Drawing.Point(128, 330);
             this.btnCroissant.Name = "btnCroissant";
-            this.btnCroissant.Size = new System.Drawing.Size(104, 35);
+            this.btnCroissant.Size = new System.Drawing.Size(110, 100);
             this.btnCroissant.TabIndex = 3;
-            this.btnCroissant.Text = "Croissant";
+            this.btnCroissant.Text = "Croissant Mixto";
             this.btnCroissant.UseVisualStyleBackColor = true;
             this.btnCroissant.Click += new System.EventHandler(this.btnCroissant_Click);
             // 
             // btnCobrar
             // 
             this.btnCobrar.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCobrar.Location = new System.Drawing.Point(69, 289);
+            this.btnCobrar.Location = new System.Drawing.Point(178, 350);
             this.btnCobrar.Name = "btnCobrar";
             this.btnCobrar.Size = new System.Drawing.Size(104, 35);
             this.btnCobrar.TabIndex = 4;
@@ -94,7 +108,7 @@
             // btnEliminar
             // 
             this.btnEliminar.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnEliminar.Location = new System.Drawing.Point(53, 222);
+            this.btnEliminar.Location = new System.Drawing.Point(35, 350);
             this.btnEliminar.Name = "btnEliminar";
             this.btnEliminar.Size = new System.Drawing.Size(137, 35);
             this.btnEliminar.TabIndex = 5;
@@ -108,10 +122,165 @@
             this.pnl1.Controls.Add(this.btnEliminar);
             this.pnl1.Controls.Add(this.btnCobrar);
             this.pnl1.Controls.Add(this.lblTotal);
-            this.pnl1.Location = new System.Drawing.Point(564, 1);
+            this.pnl1.Controls.Add(this.dgvPedido);
+            this.pnl1.Location = new System.Drawing.Point(476, 1);
             this.pnl1.Name = "pnl1";
-            this.pnl1.Size = new System.Drawing.Size(242, 445);
+            this.pnl1.Size = new System.Drawing.Size(330, 445);
             this.pnl1.TabIndex = 6;
+            // 
+            // btnAmericano
+            // 
+            this.btnAmericano.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAmericano.Location = new System.Drawing.Point(128, 12);
+            this.btnAmericano.Name = "btnAmericano";
+            this.btnAmericano.Size = new System.Drawing.Size(110, 100);
+            this.btnAmericano.TabIndex = 7;
+            this.btnAmericano.Text = "Americano";
+            this.btnAmericano.UseVisualStyleBackColor = true;
+            this.btnAmericano.Click += new System.EventHandler(this.btnAmericano_Click);
+            // 
+            // btnCapuchino
+            // 
+            this.btnCapuchino.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCapuchino.Location = new System.Drawing.Point(244, 12);
+            this.btnCapuchino.Name = "btnCapuchino";
+            this.btnCapuchino.Size = new System.Drawing.Size(110, 100);
+            this.btnCapuchino.TabIndex = 8;
+            this.btnCapuchino.Text = "Capuchino";
+            this.btnCapuchino.UseVisualStyleBackColor = true;
+            this.btnCapuchino.Click += new System.EventHandler(this.btnCapuchino_Click);
+            // 
+            // btnLatte
+            // 
+            this.btnLatte.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLatte.Location = new System.Drawing.Point(360, 12);
+            this.btnLatte.Name = "btnLatte";
+            this.btnLatte.Size = new System.Drawing.Size(110, 100);
+            this.btnLatte.TabIndex = 9;
+            this.btnLatte.Text = "Latte";
+            this.btnLatte.UseVisualStyleBackColor = true;
+            this.btnLatte.Click += new System.EventHandler(this.btnLatte_Click);
+            // 
+            // btnChocolateCaliente
+            // 
+            this.btnChocolateCaliente.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChocolateCaliente.Location = new System.Drawing.Point(128, 118);
+            this.btnChocolateCaliente.Name = "btnChocolateCaliente";
+            this.btnChocolateCaliente.Size = new System.Drawing.Size(110, 100);
+            this.btnChocolateCaliente.TabIndex = 10;
+            this.btnChocolateCaliente.Text = "Chocolate Caliente";
+            this.btnChocolateCaliente.UseVisualStyleBackColor = true;
+            this.btnChocolateCaliente.Click += new System.EventHandler(this.btnChocolateCaliente_Click);
+            // 
+            // btnInfusion
+            // 
+            this.btnInfusion.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInfusion.Location = new System.Drawing.Point(244, 118);
+            this.btnInfusion.Name = "btnInfusion";
+            this.btnInfusion.Size = new System.Drawing.Size(110, 100);
+            this.btnInfusion.TabIndex = 11;
+            this.btnInfusion.Text = "Té / Infusión";
+            this.btnInfusion.UseVisualStyleBackColor = true;
+            this.btnInfusion.Click += new System.EventHandler(this.btnInfusion_Click);
+            // 
+            // btnJugo
+            // 
+            this.btnJugo.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnJugo.Location = new System.Drawing.Point(360, 118);
+            this.btnJugo.Name = "btnJugo";
+            this.btnJugo.Size = new System.Drawing.Size(110, 100);
+            this.btnJugo.TabIndex = 12;
+            this.btnJugo.Text = "Jugo de Fruta";
+            this.btnJugo.UseVisualStyleBackColor = true;
+            this.btnJugo.Click += new System.EventHandler(this.btnJugo_Click);
+            // 
+            // btnFrapuccino
+            // 
+            this.btnFrapuccino.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFrapuccino.Location = new System.Drawing.Point(12, 224);
+            this.btnFrapuccino.Name = "btnFrapuccino";
+            this.btnFrapuccino.Size = new System.Drawing.Size(110, 100);
+            this.btnFrapuccino.TabIndex = 13;
+            this.btnFrapuccino.Text = "Frapuccino";
+            this.btnFrapuccino.UseVisualStyleBackColor = true;
+            this.btnFrapuccino.Click += new System.EventHandler(this.btnFrapuccino_Click);
+            // 
+            // btnIcedCoffee
+            // 
+            this.btnIcedCoffee.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIcedCoffee.Location = new System.Drawing.Point(128, 224);
+            this.btnIcedCoffee.Name = "btnIcedCoffee";
+            this.btnIcedCoffee.Size = new System.Drawing.Size(110, 100);
+            this.btnIcedCoffee.TabIndex = 14;
+            this.btnIcedCoffee.Text = "Iced Coffee";
+            this.btnIcedCoffee.UseVisualStyleBackColor = true;
+            this.btnIcedCoffee.Click += new System.EventHandler(this.btnIcedCoffee_Click);
+            // 
+            // btnAguaMineral
+            // 
+            this.btnAguaMineral.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAguaMineral.Location = new System.Drawing.Point(244, 224);
+            this.btnAguaMineral.Name = "btnAguaMineral";
+            this.btnAguaMineral.Size = new System.Drawing.Size(110, 100);
+            this.btnAguaMineral.TabIndex = 15;
+            this.btnAguaMineral.Text = "Agua Mineral";
+            this.btnAguaMineral.UseVisualStyleBackColor = true;
+            this.btnAguaMineral.Click += new System.EventHandler(this.btnAguaMineral_Click);
+            // 
+            // btnGaseosa
+            // 
+            this.btnGaseosa.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGaseosa.Location = new System.Drawing.Point(360, 224);
+            this.btnGaseosa.Name = "btnGaseosa";
+            this.btnGaseosa.Size = new System.Drawing.Size(110, 100);
+            this.btnGaseosa.TabIndex = 16;
+            this.btnGaseosa.Text = "Gaseosa";
+            this.btnGaseosa.UseVisualStyleBackColor = true;
+            this.btnGaseosa.Click += new System.EventHandler(this.btnGaseosa_Click);
+            // 
+            // btnSandwichPollo
+            // 
+            this.btnSandwichPollo.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSandwichPollo.Location = new System.Drawing.Point(12, 330);
+            this.btnSandwichPollo.Name = "btnSandwichPollo";
+            this.btnSandwichPollo.Size = new System.Drawing.Size(110, 100);
+            this.btnSandwichPollo.TabIndex = 17;
+            this.btnSandwichPollo.Text = "Sandwich de Pollo";
+            this.btnSandwichPollo.UseVisualStyleBackColor = true;
+            this.btnSandwichPollo.Click += new System.EventHandler(this.btnSandwichPollo_Click);
+            // 
+            // btnMocaccino
+            // 
+            this.btnMocaccino.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMocaccino.Location = new System.Drawing.Point(12, 118);
+            this.btnMocaccino.Name = "btnMocaccino";
+            this.btnMocaccino.Size = new System.Drawing.Size(110, 100);
+            this.btnMocaccino.TabIndex = 18;
+            this.btnMocaccino.Text = "Mocaccino";
+            this.btnMocaccino.UseVisualStyleBackColor = true;
+            this.btnMocaccino.Click += new System.EventHandler(this.btnMocaccino_Click);
+            // 
+            // btnEmpanada
+            // 
+            this.btnEmpanada.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEmpanada.Location = new System.Drawing.Point(244, 330);
+            this.btnEmpanada.Name = "btnEmpanada";
+            this.btnEmpanada.Size = new System.Drawing.Size(110, 100);
+            this.btnEmpanada.TabIndex = 19;
+            this.btnEmpanada.Text = "Empanada";
+            this.btnEmpanada.UseVisualStyleBackColor = true;
+            this.btnEmpanada.Click += new System.EventHandler(this.btnEmpanada_Click);
+            // 
+            // btnMuffin
+            // 
+            this.btnMuffin.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMuffin.Location = new System.Drawing.Point(360, 330);
+            this.btnMuffin.Name = "btnMuffin";
+            this.btnMuffin.Size = new System.Drawing.Size(110, 100);
+            this.btnMuffin.TabIndex = 20;
+            this.btnMuffin.Text = "Muffin";
+            this.btnMuffin.UseVisualStyleBackColor = true;
+            this.btnMuffin.Click += new System.EventHandler(this.btnMuffin_Click);
             // 
             // FrmCaja
             // 
@@ -119,12 +288,26 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(232)))), ((int)(((byte)(208)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnMuffin);
+            this.Controls.Add(this.btnEmpanada);
+            this.Controls.Add(this.btnMocaccino);
+            this.Controls.Add(this.btnSandwichPollo);
+            this.Controls.Add(this.btnGaseosa);
+            this.Controls.Add(this.btnAguaMineral);
+            this.Controls.Add(this.btnIcedCoffee);
+            this.Controls.Add(this.btnFrapuccino);
+            this.Controls.Add(this.btnJugo);
+            this.Controls.Add(this.btnInfusion);
+            this.Controls.Add(this.btnChocolateCaliente);
+            this.Controls.Add(this.btnLatte);
+            this.Controls.Add(this.btnCapuchino);
+            this.Controls.Add(this.btnAmericano);
             this.Controls.Add(this.pnl1);
             this.Controls.Add(this.btnCroissant);
             this.Controls.Add(this.btnCafe);
-            this.Controls.Add(this.dgvPedido);
             this.Name = "FrmCaja";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FrmCaja_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvPedido)).EndInit();
             this.pnl1.ResumeLayout(false);
             this.pnl1.PerformLayout();
@@ -141,6 +324,20 @@
         private System.Windows.Forms.Button btnCobrar;
         private System.Windows.Forms.Button btnEliminar;
         private System.Windows.Forms.Panel pnl1;
+        private System.Windows.Forms.Button btnAmericano;
+        private System.Windows.Forms.Button btnCapuchino;
+        private System.Windows.Forms.Button btnLatte;
+        private System.Windows.Forms.Button btnChocolateCaliente;
+        private System.Windows.Forms.Button btnInfusion;
+        private System.Windows.Forms.Button btnJugo;
+        private System.Windows.Forms.Button btnFrapuccino;
+        private System.Windows.Forms.Button btnIcedCoffee;
+        private System.Windows.Forms.Button btnAguaMineral;
+        private System.Windows.Forms.Button btnGaseosa;
+        private System.Windows.Forms.Button btnSandwichPollo;
+        private System.Windows.Forms.Button btnMocaccino;
+        private System.Windows.Forms.Button btnEmpanada;
+        private System.Windows.Forms.Button btnMuffin;
     }
 }
 
--- SisGestionCafeteriaBuenGranito/FrmCaja.cs
+++ SisGestionCafeteriaBuenGranito/FrmCaja.cs
@@ -87,12 +87,12 @@ namespace GranitoPOS // Asegúrate que esto coincida con el nombre que elegiste
 
         private void btnCafe_MouseClick(object sender, MouseEventArgs e)
         {
-            AgregarProducto("Café Americano", 8.50m);
+            AgregarProducto("Café Americano", 5.00m);
         }
 
         private void btnCroissant_Click(object sender, EventArgs e)
         {
-            AgregarProducto("Croissant Mixto", 12.00m);
+            AgregarProducto("Croissant Mixto", 8.00m);
         }
 
         private void btnCobrar_Click(object sender, EventArgs e)
@@ -128,5 +128,79 @@ namespace GranitoPOS // Asegúrate que esto coincida con el nombre que elegiste
             }
         }
 
+        private void FrmCaja_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAmericano_Click(object sender, EventArgs e)
+        {
+            AgregarProducto("Café Americano", 6.00m);
+        }
+
+        private void btnCapuchino_Click(object sender, EventArgs e)
+        {
+            AgregarProducto("Capuchino", 8.50m);
+        }
+
+        private void btnLatte_Click(object sender, EventArgs e)
+        {
+            AgregarProducto("Café Latte", 9.00m);
+        }
+
+        private void btnMocaccino_Click(object sender, EventArgs e)
+        {
+            AgregarProducto("Mocaccino", 10.00m);
+        }
+
+        private void btnChocolateCaliente_Click(object sender, EventArgs e)
+        {
+            AgregarProducto("Chocolate Caliente", 9.50m);
+        }
+
+        private void btnInfusion_Click(object sender, EventArgs e)
+        {
+            AgregarProducto("Infusión", 4.00m);
+        }
+
+        private void btnJugo_Click(object sender, EventArgs e)
+        {
+            AgregarProducto("Jugo de Fruta", 7.00m);
+        }
+
+        private void btnFrapuccino_Click(object sender, EventArgs e)
+        {
+            AgregarProducto("Frapuccino", 12.00m);
+        }
+
+        private void btnIcedCoffee_Click(object sender, EventArgs e)
+        {
+            AgregarProducto("Iced Coffee", 8.00m);
+        }
+
+        private void btnAguaMineral_Click(object sender, EventArgs e)
+        {
+            AgregarProducto("Agua Mineral", 2.00m);
+        }
+
+        private void btnGaseosa_Click(object sender, EventArgs e)
+        {
+            AgregarProducto("Gaseosa", 3.00m);
+        }
+
+        private void btnSandwichPollo_Click(object sender, EventArgs e)
+        {
+            AgregarProducto("Sandwich de Pollo", 12.00m);
+        }
+
+        private void btnEmpanada_Click(object sender, EventArgs e)
+        {
+            AgregarProducto("Empanada", 5.00m);
+        }
+
+        private void btnMuffin_Click(object sender, EventArgs e)
+        {
+            AgregarProducto("Muffin", 4.50m);
+        }
     }
 }
